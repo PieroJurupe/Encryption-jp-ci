443e70d7ce10694b3924acd8fcc6604c22087547
Joseph Antonio Carita Mamani <joseph.cmvd@gmail.com>
2023-08-26 18:23:15 -0500

    project init

---
+++ src/main/java/com/encryption/encryptionjpci/EncryptionJpCi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.encryption.encryptionjpci;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EncryptionJpCi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
